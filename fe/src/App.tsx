7663a2c9eed3884a67691abe65ab27873c62633d
ghbrooks <george@ghbrooks.net>
2024-11-26 17:38:07 +0000

    fe start

---
+++ fe/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "@mui/material";
+
+function App() {
+  return <Button>hello</Button>;
+}
+
+export default App;
